be67d60800189cbf422de076cc7ddd39bc215fc8
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-05-02 06:50:57 +0100

    ADDED: useFetch custom hook

---
+++ src/hooks/use-fetch.js
@@ -0,0 +1,24 @@
+const useFetch = async (url, requestBody) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const options = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(requestBody),
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+export default useFetch;
--- src/pages/search-page.jsx
+++ src/pages/search-page.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Header from '../components/Header/Header'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header/Header';
 import Filters from '../components/Filters/Filters';
+import useFetch from '../hooks/use-fetch';
 
 const SearchPage = () => {
-  return (
-    <div>
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await useFetch("https://api.weekday.technology/adhoc/getSampleJdJSON", { limit: 10, offset: 10 });
+                setData(res);
+                console.log(res); 
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchData();
+    }, [])
+    return (
+      <div>
         <Header />
         <Filters />
-    </div>
-  )
-}
+      </div>
+    );
+  }
+  
 
 export default SearchPage;
